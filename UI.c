1e0e03ece35bc4e35598b984cb808a00f3284d5e
Dr. Sergio Varela <ingvarelasantos@gmail.com>
2025-10-30 17:48:28 -0700

    Create UI.c

---
+++ UI.c
@@ -0,0 +1,23 @@
+// Scrollable log (takes upper half of the screen)
+    Evas_Object *scroller = elm_scroller_add(ad->box);
+    evas_object_size_hint_weight_set(scroller, EVAS_HINT_EXPAND, 0.5);
+    evas_object_size_hint_align_set(scroller, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_box_pack_end(ad->box, scroller);
+
+    ad->entry_log = elm_entry_add(scroller);
+    elm_entry_scrollable_set(ad->entry_log, EINA_TRUE);
+    elm_entry_editable_set(ad->entry_log, EINA_FALSE);
+    elm_entry_line_wrap_set(ad->entry_log, ELM_WRAP_CHAR);
+    elm_object_text_set(ad->entry_log, "Press 'Show Capabilities' or 'Start Live Capture'...");
+    elm_object_content_set(scroller, ad->entry_log);
+    evas_object_show(ad->entry_log);
+    evas_object_show(scroller);
+
+    // Image view (bottom half)
+    ad->img_view = elm_image_add(ad->box);
+    elm_image_resizable_set(ad->img_view, EINA_TRUE, EINA_TRUE);
+    elm_image_aspect_fixed_set(ad->img_view, EINA_FALSE);
+    evas_object_size_hint_weight_set(ad->img_view, EVAS_HINT_EXPAND, 0.5);
+    evas_object_size_hint_align_set(ad->img_view, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_box_pack_end(ad->box, ad->img_view);
+    evas_object_hide(ad->img_view);
