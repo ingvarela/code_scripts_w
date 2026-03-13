03d5a8fc85804dd8f6369fe67ea291e91d43654a
Dr. Sergio Varela <ingvarelasantos@gmail.com>
2026-03-12 20:41:50 -0700

    Create type09.ts

---
+++ type09.ts
@@ -0,0 +1,28 @@
+return <div
+  className="mock-answer-container"
+  style={{
+    position: "absolute",
+    top: "60%",
+    right: "1%",
+    width: "27%",
+    height: "28%",
+    maxHeight: "28%",
+    backgroundColor: "rgba(0, 0, 0, 0.65)",
+    color: "white",
+    fontSize: "30px",
+    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+    fontFamily: "'Samsung Sans', Arial, sans-serif",
+    clipPath: `polygon(
+      0 5%,
+      5% 0,
+      100% 0,
+      100% 95%,
+      95% 100%,
+      0% 100%
+    )`,
+    boxSizing: "border-box",
+    overflow: "hidden",
+    display: "flex",
+    flexDirection: "column",
+  }}
+>
