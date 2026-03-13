1e1057f683caab39b24c5adb7e1eee05797ecfd5
Dr. Sergio Varela <ingvarelasantos@gmail.com>
2026-03-12 20:40:18 -0700

    Create type05.ts

---
+++ type05.ts
@@ -0,0 +1,13 @@
+<div
+  ref={chatMessagesRef}
+  style={{
+    flex: 1,
+    minHeight: 0,
+    overflowY: "auto",
+    overflowX: "hidden",
+    scrollbarWidth: "none",
+    scrollBehavior: "smooth",
+    wordBreak: "break-word",
+    overflowWrap: "anywhere",
+  }}
+>
