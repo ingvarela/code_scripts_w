68f9d635559d910bc6091ee40145b9dd202ee756
Dr. Sergio Varela <ingvarelasantos@gmail.com>
2026-03-12 20:40:40 -0700

    Create type06.ts

---
+++ type06.ts
@@ -0,0 +1,10 @@
+style={{
+  padding: "10px",
+  paddingBottom: "15px",
+  paddingTop: "15px",
+  color: message.role === "user" ? "gray" : "white",
+  borderBottom: "solid 1px gray",
+  wordBreak: "break-word",
+  overflowWrap: "anywhere",
+  boxSizing: "border-box",
+}}
