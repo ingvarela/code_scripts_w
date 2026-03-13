d667072377f723cd6102d48109800930996b551e
Dr. Sergio Varela <ingvarelasantos@gmail.com>
2026-03-12 20:42:10 -0700

    Create type10.ts

---
+++ type10.ts
@@ -0,0 +1,18 @@
+<div
+  style={{
+    padding: "10px",
+    paddingBottom: "15px",
+    paddingTop: "15px",
+    color: "gray",
+    borderBottom: "solid 1px gray",
+    overflowY: "auto",
+    overflowX: "hidden",
+    wordBreak: "break-word",
+    overflowWrap: "anywhere",
+    boxSizing: "border-box",
+    flex: 1,
+    minHeight: 0,
+  }}
+>
+  <div>{content}</div>
+</div>
